c6975a444f1a496d54d05d4e50d3825dfc4ea0b0
eddie kwon <eddykwon@naver.com>
2017-06-02 22:26:24 +0900

    Initial Commit

---
+++ MoneyRank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyRank
+//
+//  Created by eddie kwon on 6/2/17.
+//  Copyright © 2017 hallomuze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
