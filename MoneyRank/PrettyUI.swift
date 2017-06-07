beb962cd7698c9ffed6178a16301d818e5d9d6a3
skkwon <skkwon.partner@iloen.com>
2017-06-07 18:23:15 +0900

    추후 ref_code - applyAttrLabelAndGetHeight 삭제함

--- MoneyRank/PrettyUI.swift
+++ MoneyRank/PrettyUI.swift
@@ -9,44 +9,7 @@
 import UIKit
 
 class PrettyUI: NSObject {
- 
-    class func applyAttrLabelAndGetHeight(aString: String , label aLabel:UILabel , lineSpacing aSpacing: CGFloat, width aWidth:CGFloat) -> CGFloat {
-        
-        // prepare parameters
-        
-        let aFont = aLabel.font
-        let paraStyle = NSMutableParagraphStyle()
-        paraStyle.lineSpacing = aSpacing
-        paraStyle.alignment = NSTextAlignment.center
-        
-        let attrString = NSMutableAttributedString(string: aString)
-        let range = NSMakeRange(0, attrString.length)
-        
-        
-        // apply font 폰트 추가
-        attrString.addAttribute(NSFontAttributeName, value: aFont, range: range)
-        
-        // apply line spacing 라인스페이싱
-        attrString.addAttribute(NSParagraphStyleAttributeName, value: paraStyle, range: range)
-        
-        // apply color ( option )  색상 속성 ( 옵션 )
-        //attrString.addAttribute(NSBackgroundColorAttributeName, value:UIColor.yellow,  range: NSRange(location: 1, length:3 ))
-        
-        
-        // put everything into aLabel
-        aLabel.attributedText = attrString
-        aLabel.numberOfLines = 0 //crucial!!  매우 중요.
-      
-        let myoptions = unsafeBitCast(
-            NSStringDrawingOptions.usesLineFragmentOrigin.rawValue |
-                NSStringDrawingOptions.usesFontLeading.rawValue, to:
-            NSStringDrawingOptions.self )
-        let retHeight = attrString.boundingRect(with: CGSize(width:aWidth, height:CGFloat.infinity), options: myoptions, context: nil).height
-        
-        return retHeight
-    }
-}
-
+  
 // 너무 잘 동작하는데.
 // https://stackoverflow.com/questions/37018916/swift-async-load-image
 
